a74720fe4235a1eb44fedd640fc74aa05a596a4f
fransImanuel <fransimanuel99@gmail.com>
2021-11-18 18:15:08 +0700

    graph

--- Graph/main.go
+++ Graph/main.go
@@ -12,17 +12,76 @@ type Vertex struct {
 }
 
 func (g *Graph) AddVertex(key int) {
-	newVertex := &Vertex{key: key}
-	g.vertices = append(g.vertices, newVertex)
+	if contains(g.vertices, key) {
+		err := fmt.Errorf("Vertext %v not added because it is an existing key", key)
+		fmt.Println(err.Error())
+	} else {
+		g.vertices = append(g.vertices, &Vertex{key: key})
+	}
+}
+
+func (g *Graph) Print() {
+	for _, v := range g.vertices {
+		fmt.Printf("\nVertex %d : ", v.key)
+		for _, k := range v.adjency {
+			fmt.Printf(" %d ", k.key)
+		}
+	}
+}
+
+//AddEdge adds an edge to the graph
+func (g *Graph) AddEdge(from, to int) {
+	//get vertex
+	fromVertex := g.getVertex(from)
+	toVertex := g.getVertex(to)
+	// check error
+	if fromVertex == nil || toVertex == nil {
+		err := fmt.Errorf("Invalid Edge( %v --> %v )", from, to)
+		fmt.Println(err.Error())
+	} else if contains(fromVertex.adjency, to) {
+		err := fmt.Errorf("Invalid Edge( %v --> %v )", from, to)
+		fmt.Println(err.Error())
+	} else {
+		//add edge
+		fromVertex.adjency = append(fromVertex.adjency, toVertex)
+	}
+}
+
+//getVertex returns a pointer to the vertex with a key integer
+func (g *Graph) getVertex(k int) *Vertex {
+	for i, v := range g.vertices {
+		if v.key == k {
+			return g.vertices[i]
+		}
+	}
+	return nil
+}
+
+func contains(s []*Vertex, k int) bool {
+	for _, v := range s {
+		if k == v.key {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
 	graph := &Graph{}
 
-	graph.AddVertex(1)
-	graph.AddVertex(2)
-	graph.AddVertex(3)
-	graph.AddVertex(4)
+	for i := 0; i < 5; i++ {
+		graph.AddVertex(i)
 
-	fmt.Println(graph.vertices[0])
+	}
+
+	// graph.AddVertex(0)
+	// graph.AddVertex(0)
+	// graph.AddVertex(0)
+
+	// fmt.Println(graph.vertices[0])
+
+	graph.AddEdge(1, 2)
+	graph.AddEdge(1, 2)
+	graph.AddEdge(6, 2)
+	graph.Print()
 }
